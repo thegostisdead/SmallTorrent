c68eaecb09321aa8338c960ef8e6e297e40d8bf6
thegostisdead <thegostisdead@gmail.com>
2023-09-27 17:01:41 +0200

    refactor: cleanner way

--- src/test.js
+++ src/test.js
@@ -1,11 +1,7 @@
 
-function isNumeric(c) {
-    return "0" <= c && c <= "9";
-}
+function isNumeric(c) { return "0" <= c && c <= "9"; }
 
-function isAlpha(c) {
-    return ("a" <= c && c <= "z") || ("A" <= c && c <= "Z");
-}
+function isAlpha(c) { return ("a" <= c && c <= "z") || ("A" <= c && c <= "Z");}
 
 function lexer(text) {
     const tokens = []
@@ -47,7 +43,8 @@ class Parser {
 
     constructor(text) {
 
-        this.text = text
+        this.tokens = lexer(text)
+        this.currentTokenIndex = 0
         this.parsed = [];
 
         // registers 
@@ -56,10 +53,11 @@ class Parser {
 
         this.string_len_buffer = [];
         this.string_buffer = []
-        this.remaining_string = -1
+        this.remaining_string = null
+        this.string_buffer_head = 0
 
         this.int_buffer = [];
-        this.negative_int = false;
+        // this.negative_int = false;
         
 
         this.dict = {}
@@ -69,95 +67,105 @@ class Parser {
         
     }
 
-    parseArray(tokens) {
+    
 
-        this.array_buffer = []
-        this.array_mode = true
-        console.log("Parsing an array")
+    parseString() {
 
+        const currentToken = this.tokens.at(this.currentTokenIndex)
+        console.log("Reading : %s", currentToken.value )
+        
+        switch (true) {
+            
+            case currentToken.type === "digit" :
+                console.log("readed string len digit")
+                this.string_len_buffer.push(currentToken.value)
+                this.currentTokenIndex += 1;
+                return this.parseString()
+
+            case currentToken.type === "stringSep" :  
+                console.log("readed stringSep")
+                this.remaining_string = parseInt(this.string_len_buffer.join(""))
+                console.log("string should have a lenght of %d", this.remaining_string)
+
+                this.currentTokenIndex += 1;
+                return this.parseString()
+
+            case currentToken.type === "char" :
+
+                const needToStop = this.string_buffer_head + 1  === this.remaining_string
+      
+
+                if (!needToStop) {
+                    this.currentTokenIndex += 1;
+                    this.string_buffer_head += 1;
+                    this.string_buffer.push(currentToken.value)
+                    return this.parseString()
+
+                } else {
+                    
+                    // End of string
+                    this.string_buffer.push(currentToken.value)
+                    this.parsed.push({"string" : this.string_buffer.join("")})
+                    this.currentTokenIndex += 1;
+
+                    // reset buff 
+                    this.string_len_buffer = [];
+                    this.string_buffer = []
+                    this.remaining_string = null
+                    this.string_buffer_head = 0
+
+                    return this.parse()
+                }
+
+        }
 
     }
 
-    parseString(tokens) {
-        console.log("Parsing an string")
-        console.log(`String Buff : ${this.string_buffer} : remaing string : ${this.remaining_string}`)
+    parseInt(){
 
-        const currentToken = tokens[0]
+        const currentToken = this.tokens.at(this.currentTokenIndex)
+        console.log("Reading : %s", currentToken.value )
 
-        if (currentToken === undefined) {
-            this.parsed.push({"string" : this.string_buffer.join("")})
-            return
+        
+        switch (true) {
+
+            case currentToken.value === "-":
+                console.log("readed negative sign")
+                this.int_buffer.unshift("-")
+                this.currentTokenIndex += 1
+                return this.parseInt()
+
+            case currentToken.value === "e" :
+                console.log("readed end of int")
+                this.parsed.push({"int" : parseInt(this.int_buffer.join(""))})
+                this.int_buffer = []
+                this.currentTokenIndex += 1
+                return this.parse()
+                
+
+            case currentToken.type === "digit" :
+                console.log("reading a digit")   
+                this.int_buffer.push(currentToken.value)
+                this.currentTokenIndex += 1
+                return this.parseInt()
         }
+       
 
-        if (currentToken.type === "stringSep") {
-            this.remaining_string = parseInt(this.string_len_buffer.join(""))
-            // reset 
-            this.string_len_buffer = [] 
-            return this.parseString(tokens.splice(1,))
-
-        } else if (currentToken.type === "digit") {
-            this.string_len_buffer.push(currentToken.value)
-            
-        } else if (currentToken.type === "char") {
-
-            this.string_buffer.push(currentToken.value)
-            this.remaining_string -= 1
-     
-            if (this.remaining_string >= 1 ) {
-                return this.parseString(tokens.splice(1,))
-            } else {
-                console.log("Adding string")
-                this.parsed.push({"string" : this.string_buffer.join("")})
-                return
-                // end of string
-            }
-            
-        }
-
-
-    }
-
-    parseInt(tokens){
-        console.log("Parsing an integer")
-        console.log(`Int Buff : ${this.int_buffer} : isNegative : ${this.negative_int}`)
-        console.log(tokens)
-
-        const currentToken = tokens[0]
-
-        if (currentToken)
-
-        console.log("Current Token : ", currentToken)
-
-        if (currentToken.value === '-') {
-            console.log("negative int ")
-            this.negative_int = true 
-            return this.parseInt(tokens.slice(1,))
-
-        } else if (currentToken.type === "digit") {
-            this.int_buffer.push(currentToken.value)
-            return this.parseInt(tokens.slice(1,))
-
-        } else if (currentToken.value === "e") {
-            // end of int 
-            const fullString = this.negative_int ? "-" + this.int_buffer.join('') : this.int_buffer.join('')
-
-            // reset 
-            this.negative_int = false
-            this.int_buffer = []
-
-            if (this.array_mode) {
-                this.array_buffer.push(parseInt(fullString))
-                return 
-            }
-            console.log("Adding integer to parsed -> %d", parseInt(fullString))
-            this.parsed.push({"number" : parseInt(fullString)})
-        }
     }
 
     parseDict(tokens) {
         console.log("Parsing a Dictionary")
         console.log(`Int Buff : ${this.int_buffer} : isNegative : ${this.negative_int}`)
         console.log(tokens)
+    }
+
+    parseArray(tokens) {
+        this.array_buffer = []
+        this.array_mode = true
+        console.log("Parsing an array")
+        const x = this.parse(tokens)
+        console.log(x)
+
     }
 
     // decodeBencode("5:hello") -> "hello"
@@ -169,30 +177,39 @@ class Parser {
     // decodeBencode("d3:foo3:bar5:helloi52ee") -> {"foo":"bar","hello":52}
 
     parse() {
+        console.log("parsing...")
+        console.log(this.parsed)
 
-        const tokenStream = lexer(this.text)
-    
-        const currentToken = tokenStream[0]
-        const lastToken = tokenStream.at(-1)
+        const lastToken = this.tokens.at(-1)
+        const currentToken = this.tokens.at(this.currentTokenIndex)
+
+        if (currentToken === undefined) {
+            console.log("EOF")
+            return this.parsed
+        }
+
+        this.currentTokenIndex += 1 
 
         if(currentToken.type === "digit") {
+
             this.string_len_buffer.push(currentToken.value)
-            return this.parseString(tokenStream.slice(1,))
+            return this.parseString()
 
         } else if (currentToken.type === "char") {
-
+            
             if (currentToken.value === "i") {
-                return this.parseInt(tokenStream.slice(1,))
+                return this.parseInt()
 
             } else if (currentToken.value === "l" && lastToken.value === "e") {
                 this.array_mode = true 
-                return this.parseArray(tokenStream.slice(1,-1))
+                return this.parseArray(this.tokens.slice(1,-1))
             } else if (currentToken.value === "d" && lastToken.value === "e") {
-                return this.parseDict(tokenStream.slice(1,-1))
+                return this.parseDict(this.tokens.slice(1,-1))
             }
 
         } else if (currentToken === undefined) {
             // end 
+            console.log("EOF")
         }
 
         return this.parsed
@@ -214,6 +231,10 @@ function decodeBencode(bencodedValue) {
     return parser.stringify()
 }
 
-decodeBencode("5:hello")
+
 console.log("--------------------------")
+decodeBencode("i52ei20ei-8e")
+
 //decodeBencode("i-52e")
+// decodeBencode("5:hello3:aze6:dorian")
+// decodeBencode("l5:helloi52ee")
